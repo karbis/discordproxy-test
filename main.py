8851abe3b1ab129352cfad67f4d83853a17745f1
karbis <102873508+karbis@users.noreply.github.com>
2023-06-21 15:58:19 +0300

    Ummmmm mmm

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse, Response
-from fastapi.middleware.cors import CORSMiddleware
 import httpx
+from httpx import AsyncClient
+from fastapi import Request
+from fastapi.responses import StreamingResponse
+from starlette.background import BackgroundTask
 
-app = FastAPI(root_path="https://discord.com")
+app = FastAPI()
 
 # CORS Configuration
 origins = ["*",
@@ -18,6 +19,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def index():
-    return RedirectResponse(url="https://discord.com")
+HTTP_SERVER = AsyncClient(base_url="https://discord.com/")
+
+async def _reverse_proxy(request: Request):
+    url = httpx.URL(path=request.url.path, query=request.url.query.encode("utf-8"))
+    rp_req = HTTP_SERVER.build_request(
+        request.method, url, headers=request.headers.raw, content=await request.body()
+    )
+    rp_resp = await HTTP_SERVER.send(rp_req, stream=True)
+    return StreamingResponse(
+        rp_resp.aiter_raw(),
+        status_code=rp_resp.status_code,
+        headers=rp_resp.headers,
+        background=BackgroundTask(rp_resp.aclose),
+    )
+
+app.add_route("/{path:path}", _reverse_proxy, ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
