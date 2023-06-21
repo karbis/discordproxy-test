7f4136d6a6eb4238e7654d8a404aff3d7875fad2
karbis <102873508+karbis@users.noreply.github.com>
2023-06-21 15:45:49 +0300

    CHATGPT IS THE FUTURE!

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
 
 app = FastAPI()
 
-# Enable CORS for all origins
+# CORS configuration
+origins = ["*"]  # Add specific origins if needed
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
 @app.middleware("http")
-async def proxy_request(request: Request, call_next):
+async def proxy_discord(request, call_next):
+    url = f"https://discord.com{request.url.path}"
+    headers = dict(request.headers)
     try:
-        # Modify the URL as per your needs
-        url = f"https://discord.com{request.url.path}"
-
-        # Forward the request to Discord
         async with httpx.AsyncClient() as client:
             response = await client.request(
-                method=request.method,
-                url=url,
-                headers=dict(request.headers),
-                data=await request.body(),
+                request.method, url, headers=headers, data=await request.body()
             )
-
-        # Return the response from Discord
-        return response.content
-    except httpx.RequestError as e:
-        raise HTTPException(status_code=502, detail=str(e))
+            response.raise_for_status()
+            return response.content
+    except httpx.HTTPError as e:
+        raise HTTPException(status_code=e.response.status_code)
