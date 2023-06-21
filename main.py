b44be51465d5255befcd53d5f38caf95840155c5
karbis <102873508+karbis@users.noreply.github.com>
2023-06-21 16:26:33 +0300

    PLS MY BRUdda

--- main.py
+++ main.py
@@ -1,54 +1,38 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse, Response
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import httpx
+import requests
 
 app = FastAPI()
 
-# CORS Configuration
-origins = ["*",
-    # Add more allowed origins as needed
-]
-
+# Enable CORS for all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_origins=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
+@app.get("/channels")
+def get_channels(id: int, authorization: str):
+    url = f"https://discord.com/api/v10/guilds/{id}/channels"
+    headers = {"Authorization": authorization}
 
-@app.get("/")
-def index():
-    return RedirectResponse(url="https://discord.com")
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
 
+@app.post("/vc")
+def set_vc(serverId: int, userId: int, channel_id: int, authorization: str):
+    url = f"https://discord.com/api/v10/guilds/{serverId}/members/{userId}"
+    headers = {"Authorization": authorization}
+    json_data = {"channel_id": channel_id}
 
-@app.get("/api/{path:path}")
-@app.post("/api/{path:path}")
-@app.put("/api/{path:path}")
-@app.patch("/api/{path:path}")
-@app.delete("/api/{path:path}")
-@app.options("/api/{path:path}")
-async def proxy_request(path: str, request: Request):
-    discord_url = f"https://discord.com/api/{path}"
+    response = requests.patch(url, headers=headers, json=json_data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
 
-    headers = {key: value for key, value in request.headers.items()}
-
-    async with httpx.AsyncClient() as client:
-        if request.method == "GET":
-            response = await client.get(discord_url, headers=headers)
-        elif request.method == "POST":
-            response = await client.post(discord_url, headers=headers, data=request.body())
-        elif request.method == "PUT":
-            response = await client.put(discord_url, headers=headers, data=request.body())
-        elif request.method == "PATCH":
-            response = await client.patch(discord_url, headers=headers, data=request.body())
-        elif request.method == "DELETE":
-            response = await client.delete(discord_url, headers=headers, data=request.body())
-        elif request.method == "OPTIONS":
-            response = await client.options(discord_url, headers=headers)
-
-    content = response.content
-    resp = Response(content, media_type=response.headers["Content-Type"])
-    return resp
+#its not a proxy soryr i lied
