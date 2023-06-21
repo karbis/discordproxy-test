506f92b3cc5d320170b35d2e511c558bb9be677a
karbis <102873508+karbis@users.noreply.github.com>
2023-06-21 15:40:30 +0300

    asdfasdfadsfsdfadsfadsfdasfdasfdsafdsaf

--- main.py
+++ main.py
@@ -1,44 +1,34 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse, Response
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
 
 app = FastAPI()
 
+# Enable CORS for all origins
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.get("/")
-def index():
-    return RedirectResponse(url="https://discord.com")
+@app.middleware("http")
+async def proxy_request(request, call_next):
+    try:
+        # Modify the URL as per your needs
+        url = f"https://discord.com{request.url.path}"
 
+        # Forward the request to Discord
+        async with httpx.AsyncClient() as client:
+            response = await client.request(
+                method=request.method,
+                url=url,
+                headers=request.headers,
+                data=await request.body(),
+            )
 
-@app.route("/api/{path:path}", methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
-async def proxy_request(path: str, request: Request):
-    discord_url = f"https://discord.com/api/{path}"
-
-    headers = {key: value for key, value in request.headers.items()}
-
-    async with httpx.AsyncClient() as client:
-        if request.method == "GET":
-            response = await client.get(discord_url, headers=headers)
-        elif request.method == "POST":
-            response = await client.post(discord_url, headers=headers, data=request.body())
-        elif request.method == "PUT":
-            response = await client.put(discord_url, headers=headers, data=request.body())
-        elif request.method == "PATCH":
-            response = await client.patch(discord_url, headers=headers, data=request.body())
-        elif request.method == "DELETE":
-            response = await client.delete(discord_url, headers=headers, data=request.body())
-        elif request.method == "OPTIONS":
-            response = await client.options(discord_url, headers=headers)
-
-    content = response.content
-    resp = Response(content, media_type=response.headers["Content-Type"])
-    return resp
+        # Return the response from Discord
+        return response
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=502, detail=str(e))
